943924d8325fe662c36758c88d9f39d831755d97
annasuaareez <a.suarez.villaescusa@gmail.com>
2024-02-23 10:57:08 +0100

    Splash Activity Inicializada

--- app/src/main/java/com/example/appcine/Splash.kt
+++ app/src/main/java/com/example/appcine/Splash.kt
@@ -1,12 +1,22 @@
 package com.example.appcine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.appcine.R
 
 class Splash : AppCompatActivity() {
+    private val splashTime: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@Splash, MainActivity2::class.java)
+            startActivity(intent)
+        }, splashTime)
     }
 }
